069492d4bb6cfc4d0c05842de7fb57cc7bfb994c
greta <gretadoci@gmail.com>
2021-09-09 16:52:24 +0200

    fixup! Add new message into a modal
    Signed-off-by: greta <gretadoci@gmail.com>

--- src/components/Composer.vue
+++ src/components/Composer.vue
@@ -212,7 +212,10 @@
 		</div>
 	</div>
 	<Loading v-else-if="state === STATES.UPLOADING" :hint="t('mail', 'Uploading attachments …')" role="alert" />
-	<Loading v-else-if="state === STATES.SENDING" :hint="t('mail', 'Sending …')" role="alert" />
+	<Loading v-else-if="state === STATES.SENDING"
+		:hint="t('mail', 'lalalala …')"
+		role="alert"
+		class="sending-hint" />
 	<div v-else-if="state === STATES.ERROR" class="emptycontent" role="alert">
 		<h2>{{ t('mail', 'Error sending your message') }}</h2>
 		<p v-if="errorText">
@@ -941,5 +944,9 @@ export default {
 }
 .submit-message.send.primary.icon-confirm-white {
 	color: var(--color-main-background);
+}
+.sending-hint {
+	height: 50px;
+	margin-top: 50px;
 }
 </style>
